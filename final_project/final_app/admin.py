4436785a2294bc9ef49fb75133e9b3a736577a5f
Rakan-Almutiri <44053941+Rakan-Almutiri@users.noreply.github.com>
2018-12-30 12:20:42 +0300

    added models

--- final_project/final_app/admin.py
+++ final_project/final_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, Category, emp
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'price', 'quantity']
+    list_filter = ['price']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['name']
+
+
+class EmpAdmin(admin.ModelAdmin):
+    model = emp
+    list_display = ['username', 'email']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(emp, EmpAdmin)
--- final_project/final_app/models.py
+++ final_project/final_app/models.py
@@ -1,4 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
 
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(upload_to='product/images', null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    confirm = models.IntegerField(null=True, blank=True)
+    emp_create = models.ForeignKey('emp', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class emp(models.Model):
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    middile_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    pws = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=250, null=True, blank=True)
+    username = models.CharField(max_length=250, null=True, blank=True)
+    position = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+    @property
+    def fullname(self):
+        return self.first_name + self.middile_name + self.last_name

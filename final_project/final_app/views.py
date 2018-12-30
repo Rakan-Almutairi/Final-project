adc4c6b9d30e44383d2072572256bfa235521ee4
Rakan-Almutiri <44053941+Rakan-Almutiri@users.noreply.github.com>
2018-12-30 10:47:57 +0300

    done the basic

--- final_project/final_app/views.py
+++ final_project/final_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    title = 'Home Page'
+    context = {'title': title}
+    return render(request, 'home.html', context)
